74fe34b2f0b46832216dcacbdcf7052912490005
Peter Reutemann <fracpete@gmail.com>
2024-06-13 11:05:10 +1200

    prepared for release

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Changelog
 =========
 
-0.0.2 (????-??-??)
+0.0.2 (2024-06-13)
 ------------------
 
 - upgraded image-dataset-converter to 0.0.2
--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
         "image-dataset-converter-redis>=0.0.2",
         "image-dataset-converter-video>=0.0.1",
     ],
-    version="0.0.1",
+    version="0.0.2",
     author='Peter Reutemann',
     author_email='fracpete@waikato.ac.nz',
 )
